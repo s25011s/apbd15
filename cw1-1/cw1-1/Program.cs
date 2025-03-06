7f1d06602a7d9f1d0e19f7d56cd5d5835c288ee8
L1m3x <48759715+L1m3x@users.noreply.github.com>
2025-03-06 21:00:49 +0100

    avg

--- cw1-1/cw1-1/Program.cs
+++ cw1-1/cw1-1/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("abc");
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine(Average([1,2,3,4,5,10]));
+    }
+    private static float Average(int[] values)
+    {
+        float result = 0;
+        foreach (int value in values){
+            result += value;
+        }
+        return result/values.Length;
+    }
+}
