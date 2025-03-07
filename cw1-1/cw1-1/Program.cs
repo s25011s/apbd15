c4c52e5a9f5670a718ac0a5d8901aad4e495129e
L1m3x <48759715+L1m3x@users.noreply.github.com>
2025-03-07 15:10:24 +0100

    max

--- cw1-1/cw1-1/Program.cs
+++ cw1-1/cw1-1/Program.cs
@@ -3,13 +3,27 @@
     private static void Main(string[] args)
     {
         Console.WriteLine(Average([1,2,3,4,5,10]));
+        Console.WriteLine(Max([1, 2, 3, 4, 5, 10]));
     }
     private static float Average(int[] values)
     {
         float result = 0;
-        foreach (int value in values){
+        foreach (int value in values)
+        {
             result += value;
         }
-        return result/values.Length;
+        return result / values.Length;
+    }
+    private static float Max(int[] values)
+    {
+        float result = values[0];
+        foreach (int value in values)
+        {
+            if(value > result)
+            {
+                result = value;
+            }
+        }
+        return result;
     }
 }
